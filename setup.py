97c693041eb2685b5fd8487569ec7bad68e993ea
Rafał Selewońko <rafal@selewonko.com>
2018-05-28 12:16:40 +0200

    Allow specifying commandline arguments.

--- setup.py
+++ setup.py
@@ -2,11 +2,9 @@
 
 from setuptools import setup
 
-from dl import __version__
-
 setup(
     name='youtube-dl-subscriptions',
-    version=__version__,
+    version='0.3',
     description='Downloads all new videos from your YouTube subscription feeds.',
     url='https://github.com/mewfree/youtube-dl-subscriptions',
     py_modules=['dl'],
