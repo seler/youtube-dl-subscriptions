75af6f117c5f39fb0a5609ee306f6c7e9ac4a68c
Rafał Selewońko <rafal@selewonko.com>
2018-07-09 23:32:03 +0200

    Allow faking and logging options.

--- dl.py
+++ dl.py
@@ -10,11 +10,14 @@ import argparse
 import feedparser
 import opml
 import youtube_dl
-
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import IntegrityError
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 Base = declarative_base()
 
@@ -36,42 +39,58 @@ def main():
 
     parser.add_argument('--opml-file', default='subs.xml')
     parser.add_argument('--database', default='sqlite:///ytdls.db')
+    parser.add_argument('--fake', action="store_true")
     parser.add_argument('-o', '--output', metavar='TEMPLATE', dest='outtmpl',
                         help='YoutubeDL output filename template, see the '
                              'YoutubeDL "OUTPUT TEMPLATE" for all the info')
 
     args = parser.parse_args()
-    dl(args.opml_file, args.database, args.outtmpl)
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    if args.quiet:
+        logger.setLevel(logging.ERROR)
+
+    dl(args.opml_file, args.database, args.outtmpl,
+       args.fake, args.quiet, args.verbose)
 
 
-def dl(opml_filename, database, outtmpl):
+def dl(opml_filename, database, outtmpl,
+       fake=False, quiet=False, verbose=False):
     engine = create_engine(database)
     Base.metadata.create_all(engine)
     Base.metadata.bind = engine
     session = sessionmaker(bind=engine)()
 
-    channels = opml.parse(opml_filename)[0]
+    opts = {}
+    if outtmpl:
+        opts['outtmpl'] = outtmpl
+    if quiet:
+        opts['quiet'] = quiet
+    if verbose:
+        opts['verbose'] = verbose
 
-    videos = []
-
-    for i, channel in enumerate(channels):
-        feed = feedparser.parse(channel.xmlUrl)
-        for item in feed['items']:
-            url = item['link']
-            session.add(Video(url=url))
+    for channel in opml.parse(opml_filename)[0]:
+        logger.debug('Parsing channel "{}".'.format(channel.title))
+        for item in feedparser.parse(channel.xmlUrl)['items']:
+            session.add(Video(url=item['link']))
             try:
                 session.commit()
             except IntegrityError:
                 session.rollback()
             else:
-                videos.append(url)
+                msg = 'Downloading "{}" from "{}".'
+                logger.info(msg.format(item['title'], channel.title))
+                if not fake:
+                    try:
+                        download(item['link'], opts)
+                    except Exception:
+                        session.rollback()
 
-    ydl_opts = {}
-    if outtmpl:
-        ydl_opts['outtmpl'] = outtmpl
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(videos)
+def download(url, opts):
+    with youtube_dl.YoutubeDL(opts) as ydl:
+        ydl.download([url])
 
 
 if __name__ == '__main__':
