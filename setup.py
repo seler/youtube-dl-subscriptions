29ee357d44a2831ee9a5ebc1e7edc19faf0cd23d
Rafał Selewońko <rafal@selewonko.com>
2018-07-10 14:18:26 +0200

    Fix module name.

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 
 setup(
     name='youtube-dl-subscriptions',
-    version='0.3',
+    version='0.4',
     description='Downloads all new videos from your YouTube subscription feeds.',
     url='https://github.com/mewfree/youtube-dl-subscriptions',
-    py_modules=['dl'],
+    py_modules=['youtube_dl_subscriptions'],
     entry_points={'console_scripts': [
-        'youtube-dl-subscriptions = dl:main',
+        'youtube-dl-subscriptions = youtube_dl_subscriptions:main',
     ]},
     install_requires=[
         'opml',
